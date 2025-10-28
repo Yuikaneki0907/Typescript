28377329ff603416b397f7a81b8deb40f09f1879
Yuikaneki0907 <leanh1572004@gmail.com>
2025-10-28 20:44:21 +0700

    constructor

--- dist/chapter12/video114/video114.js
+++ dist/chapter12/video114/video114.js
@@ -1,3 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log("constructor");
+class Person {
+    constructor(name, age) {
+        this.name = name ?? "unknow";
+        this.age = age ?? 0;
+    }
+    tell() {
+        console.log("My name is", this.name);
+    }
+}
+const person1 = new Person("Lê", 21);
+person1.tell();
+const person2 = new Person();
--- src/chapter12/video114/video114.ts
+++ src/chapter12/video114/video114.ts
@@ -1,3 +1,27 @@
 export { };
 
 console.log("constructor")
+
+class Person {
+    name: string;
+    age: number;
+
+    //Hàm tạo constructor
+    //overload 
+    constructor();
+    constructor(name: string, age: number);
+
+    constructor(name?: string, age?: number){
+        this.name = name ?? "unknow";
+        this.age = age ?? 0;
+    }
+
+    tell(){
+        console.log("My name is", this.name)
+    }
+}
+
+const person1 = new Person("Lê", 21)
+person1.tell()
+
+const person2 = new Person()
