93137d225a6c573b296bc042ce020c80f7dfee1d
Yuikaneki0907 <leanh1572004@gmail.com>
2025-11-01 12:34:51 +0700

    import

---
+++ src/chapter14/video137/main.ts
@@ -0,0 +1,5 @@
+// import { age, demo, name } from "./test";
+
+import * as allFile from "./test";
+
+const a =  allFile.age
---
+++ src/chapter14/video137/test.ts
@@ -0,0 +1,9 @@
+const name = "lee";
+const age = 25;
+
+const demo = () => {
+
+}
+
+
+export {name, age, demo};
